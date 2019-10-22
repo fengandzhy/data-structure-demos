3dcfeb7b864d7fa94004f0b6dc8850dce6b477f3
fengandzhy <fengandzhy@gmail.com>
2019-10-23 05:58:49 +1300

    bag demo

--- datastructure-demos/datastructure-recursion/src/main/java/org/zhouhy/recursion/bags/BagsDemo.java
+++ datastructure-demos/datastructure-recursion/src/main/java/org/zhouhy/recursion/bags/BagsDemo.java
@@ -1,5 +1,6 @@
 package org.zhouhy.recursion.bags;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,11 +8,55 @@ import java.util.List;
  * */
 public class BagsDemo {
 	
-	public void bag(int[] targetArray,int targetWeight,int beginIndex,List<int[]> results){
+	public void bag(int[] targetArray,int targetWeight,int currentIndex, List<List<Integer>> results,List<Integer> result){
 		
+				
+		if(currentIndex>targetArray.length-1){
+			return;
+		}
+		
+		if(targetArray[currentIndex]<targetWeight){
+			result.add(targetArray[currentIndex]);
+			bag(targetArray,targetWeight-targetArray[currentIndex],++currentIndex,results,result);
+		}else if(targetArray[currentIndex]==targetWeight){
+			result.add(targetArray[currentIndex]);
+			results.add(result);
+		}else {
+			bag(targetArray,targetWeight,++currentIndex,results,result);
+		}			
 	}
 	
+	public void bag1(int[] targetArray,int targetWeight,int currentIndex, List<List<Integer>> results,List<Integer> result){		
+		if(currentIndex>targetArray.length-1){
+			return;
+		}		
+		if(targetArray[currentIndex]<targetWeight){
+			result.add(targetArray[currentIndex]);
+			bag(targetArray,targetWeight-targetArray[currentIndex],++currentIndex,results,result);
+			return;
+		}
+		if(targetArray[currentIndex]==targetWeight){
+			result.add(targetArray[currentIndex]);
+			results.add(result);
+			return;
+		}
+		if(targetArray[currentIndex]>targetWeight){
+			bag(targetArray,targetWeight,++currentIndex,results,result);
+			return;
+		}			
+	}	
 	
+	public void runBag(int[] targetArray,int targetWeight,List<List<Integer>> results){
+		for(int i=0;i<targetArray.length;i++){			
+			List<Integer> result = new ArrayList<>();
+			this.bag(targetArray,targetWeight,i,results,result);			
+		}
+	}
 	
-	
+	public void runBag1(int[] targetArray,int targetWeight,List<List<Integer>> results){
+		for(int i=0;i<targetArray.length;i++){			
+			List<Integer> result = new ArrayList<>();
+			this.bag1(targetArray,targetWeight,i,results,result);			
+		}
+	}
 }
---
+++ datastructure-demos/datastructure-recursion/src/test/java/org/zhouhy/recursion/bags/BagsDemoTest.java
@@ -0,0 +1,69 @@
+package org.zhouhy.recursion.bags;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BagsDemoTest {
+
+	private BagsDemo demo;
+	
+	@Before
+	public void init(){
+		System.out.println("≤‚ ‘ø™ º");
+	}
+	
+	@Test
+	public void test1() {
+		demo = new BagsDemo();
+		int[] as = new int[]{12,8,7,5};		
+		List<List<Integer>> results = new ArrayList<>();
+		demo.runBag(as, 20, results);
+		for(List<Integer> result:results){
+			for(Integer i:result){
+				System.out.print(i+",");
+			}
+			System.out.println();
+		}
+	}
+	
+	@Test
+	public void test2() {
+		demo = new BagsDemo();
+		int[] as = new int[]{12,8,7,5};		
+		List<List<Integer>> results = new ArrayList<>();
+		demo.runBag1(as, 20, results);
+		for(List<Integer> result:results){
+			for(Integer i:result){
+				System.out.print(i+",");
+			}
+			System.out.println();
+		}
+	}
+	
+	@Test
+	public void test3() {
+		demo = new BagsDemo();
+		int[] as = new int[]{12,8,7,5};		
+		List<List<Integer>> results = new ArrayList<>();
+		List<Integer> result = new ArrayList<>();
+		demo.bag2(as, 20, 0, results, result);
+		for(List<Integer> r:results){
+			for(Integer i:r){
+				System.out.print(i+",");
+			}
+			System.out.println();
+		}
+	}
+	
+	@After
+	public void destory(){
+		System.out.println("≤‚ ‘Ω· ¯");
+	}
+
+}
