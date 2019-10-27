b4e0bb5d11b87af651fab89c8d215c56d9b0ee33
fengandzhy <fengandzhy@gmail.com>
2019-10-27 23:05:42 +1300

    shell sort demo

--- datastructure-demos/datastructure-advancedsort/src/main/java/org/zhouhy/datastructure/shellsort/ShellSortDemo1.java
+++ datastructure-demos/datastructure-advancedsort/src/main/java/org/zhouhy/datastructure/shellsort/ShellSortDemo1.java
@@ -2,4 +2,18 @@ package org.zhouhy.datastructure.shellsort;
 
 public class ShellSortDemo1 {
 	
+	public void shellsort(int[] array){
+		int length = array.length;
+		int increment = length/2;
+		
+		while(increment>0){
+			for(int i=increment;i<length;i++){
+				int temp = array[i];
+				for(int j=i;j>0;j=j-increment){
+					
+				}
+			}
+		}
+	}
+	
 }
